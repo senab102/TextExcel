905e07f4703136a8724fef2e71fc5fe2b1b024a2
Hernandez <Hernandez@172.20.10.2>
2018-03-18 22:31:51 -0700

    test

--- TextExcel/src/textExcel/EmptyCell.java
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -1,5 +1,13 @@
 package textExcel;
 
-public class EmptyCell {
+public class EmptyCell implements Cell{
 
+	public String abbreviatedCellText() { // text for spreadsheet cell display, must be exactly length 10
+
+		return null;
+	}
+	
+	public String fullCellText() { // text for individual cell inspection, not truncated or padded
+		return null;
+	}
 }
